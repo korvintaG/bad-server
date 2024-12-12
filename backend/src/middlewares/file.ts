7bb944502165b5dbe101083dbb6348aeac29feb4
Sergey Korobok <korvin@kati.ru>
2024-12-12 16:55:42 +0300

    Upload file name

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -1,6 +1,7 @@
 import { Request, Express } from 'express'
 import multer, { FileFilterCallback } from 'multer'
-import { join } from 'path'
+import { join, extname } from 'path'
+import { faker } from '@faker-js/faker';
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -27,7 +28,7 @@ const storage = multer.diskStorage({
         file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        cb(null, file.originalname)
+        cb(null, faker.string.uuid()+ extname(file?.originalname))
     },
 })
 
