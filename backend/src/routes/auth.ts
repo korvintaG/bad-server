d248d773d3648da73ea7553a9ca76978bba29895
Sergey Korobok <korvin@kati.ru>
2024-12-18 17:33:09 +0300

    Repair

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -19,6 +19,6 @@ authRouter.get('/user/roles', auth, getCurrentUserRoles)
 authRouter.post('/login', login)
 authRouter.get('/token', refreshAccessToken)
 authRouter.get('/logout', logout)
-authRouter.post('/register', doubleCsrfProtection, register)
+authRouter.post('/register', register)
 
 export default authRouter
